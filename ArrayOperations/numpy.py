9a9a6159935173817e439a30e8c90d108a65e5d7
HarshadaSamane <samaneharshada04@gmail.com>
2026-09-22 13:39:30 +0530

    Added ArrayOperations folder

---
+++ ArrayOperations/numpy.py
@@ -0,0 +1,13 @@
+# Array Operations
+
+import numpy as numpy
+
+arr = np.array([
+    [10, 20, 30],
+    [40, 50, 60]
+])
+
+print("Dimentions:" , arr.ndim)
+print("Shape: ", arr.shape)
+print("Size: ", arr.size)
+print("Data type: ", arr.dtype)
